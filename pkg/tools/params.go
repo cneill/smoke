9e3ce931257b3defec0546214e7be8a439c7ce69
Charles Neill <1749665+cneill@users.noreply.github.com>
2025-08-08 16:47:59 -0500

    Splitting out Args / Params, adding GetStringSlice

---
+++ pkg/tools/params.go
@@ -0,0 +1,62 @@
+package tools
+
+type ParamType string
+
+const (
+	ParamTypeArray   ParamType = "array"
+	ParamTypeBoolean ParamType = "boolean"
+	ParamTypeNull    ParamType = "null"
+	ParamTypeNumber  ParamType = "number"
+	ParamTypeObject  ParamType = "object"
+	ParamTypeString  ParamType = "string"
+)
+
+type Param struct {
+	Key         string
+	Description string
+	Type        ParamType
+	Required    bool
+}
+
+type Params []Param
+
+func (p Params) ByKey(key string) *Param {
+	for _, param := range p {
+		if param.Key == key {
+			return &param
+		}
+	}
+
+	return nil
+}
+
+func (p Params) Keys() []string {
+	results := []string{}
+	for _, param := range p {
+		results = append(results, param.Key)
+	}
+
+	return results
+}
+
+func (p Params) RequiredKeys() []string {
+	results := []string{}
+
+	for _, param := range p {
+		if param.Required {
+			results = append(results, param.Key)
+		}
+	}
+
+	return results
+}
+
+func (p Params) Required(key string) bool {
+	for _, param := range p {
+		if param.Key == key && param.Required {
+			return true
+		}
+	}
+
+	return false
+}
