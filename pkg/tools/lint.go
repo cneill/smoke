fabca700631f89c0e07d001cfb97a38329fadae4
Charles Neill <1749665+cneill@users.noreply.github.com>
2025-08-09 20:17:24 -0500

    Adding 'path' to lint tool

--- pkg/tools/lint.go
+++ pkg/tools/lint.go
@@ -1,38 +1,69 @@
 package tools
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
 	"os/exec"
+
+	"github.com/cneill/smoke/pkg/utils"
 )
 
-// LintTool is a tool to run golangci-lint on the project directory.
+const (
+	LintPath = "path"
+)
+
 type LintTool struct {
 	ProjectPath string
 }
 
-// Name returns the name of the tool.
-func (t *LintTool) Name() string {
+func (l *LintTool) Name() string {
 	return "lint"
 }
 
-// Description returns the description of the tool.
-func (t *LintTool) Description() string {
-	return "Runs golangci-lint against the ProjectDirectory."
+func (l *LintTool) Description() string {
+	return "Runs the golangci-lint linter against the specified file/directory, or the whole project directory if a " +
+		"path is not specified."
 }
 
-// Params returns the required parameters for this tool.
-func (t *LintTool) Params() Params {
-	return Params{}
+func (l *LintTool) Params() Params {
+	return Params{
+		{
+			Key:         LintPath,
+			Description: "The path of the directory/file to lint",
+			Type:        ParamTypeString,
+			Required:    false,
+		},
+	}
 }
 
-// Run executes golangci-lint and returns its output.
-func (t *LintTool) Run(_ Args) (string, error) {
-	path := t.ProjectPath + "/..."
-	cmd := exec.Command("golangci-lint", "run", path)
+func (l *LintTool) Run(args Args) (string, error) {
+	fullPath := l.ProjectPath
+
+	if path := args.GetString(LintPath); path != nil {
+		relPath, err := utils.GetRelativePath(l.ProjectPath, *path)
+		if err != nil {
+			return "", fmt.Errorf("path error: %w", err)
+		}
+
+		fullPath = relPath
+	}
+
+	stat, err := os.Stat(fullPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to stat path %s: %w", fullPath, err)
+	}
+
+	if stat.IsDir() {
+		fullPath += "/..."
+	}
+
+	cmd := exec.Command("golangci-lint", "run", fullPath)
+	cmd.Dir = l.ProjectPath
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		slog.Warn("error from golangci-lint execution", "path", path, "error", err)
+		slog.Warn("error from golangci-lint execution", "path", fullPath, "error", err)
 	}
 
 	return string(output), nil
