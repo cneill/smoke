9ba289a9910aa9801688a577c0dbbeb71f7a5187
Charles Neill <1749665+cneill@users.noreply.github.com>
2025-08-13 17:20:01 -0500

    Moving tool constants to constants.go

--- pkg/tools/constants.go
+++ pkg/tools/constants.go
@@ -3,3 +3,16 @@ package tools
 const (
 	LineSep = "====================="
 )
+
+const (
+	ToolCreateDirectory = "create_directory"
+	ToolGitDiff         = "git_diff"
+	ToolGoFumpt         = "go_fumpt"
+	ToolGoLint          = "go_lint"
+	ToolGoTest          = "go_test"
+	ToolGrep            = "grep"
+	ToolListFiles       = "list_files"
+	ToolReadFile        = "read_file"
+	ToolReplaceLines    = "replace_lines"
+	ToolWriteFile       = "write_file"
+)
--- pkg/tools/manager.go
+++ pkg/tools/manager.go
@@ -5,19 +5,6 @@ import (
 	"log/slog"
 )
 
-const (
-	ToolCreateDirectory = "create_directory"
-	ToolGitDiff         = "git_diff"
-	ToolGoFumpt         = "go_fumpt"
-	ToolGoLint          = "go_lint"
-	ToolGoTest          = "go_test"
-	ToolGrep            = "grep"
-	ToolListFiles       = "list_files"
-	ToolReadFile        = "read_file"
-	ToolReplaceLines    = "replace_lines"
-	ToolWriteFile       = "write_file"
-)
-
 type Manager struct {
 	logger      *slog.Logger
 	ProjectPath string
